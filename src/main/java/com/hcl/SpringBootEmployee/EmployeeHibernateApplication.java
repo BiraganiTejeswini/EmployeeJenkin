c6a792260b4e9348bb8a7bf33dd4fd33931f7d6b
BiraganiTejeswini <84894642+BiraganiTejeswini@users.noreply.github.com>
2021-06-01 14:44:18 +0530

    Update EmployeeHibernateApplication.java

--- src/main/java/com/hcl/SpringBootEmployee/EmployeeHibernateApplication.java
+++ src/main/java/com/hcl/SpringBootEmployee/EmployeeHibernateApplication.java
@@ -3,12 +3,18 @@ package com.hcl.SpringBootEmployee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; 
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeHibernateApplication {
+public class EmployeeHibernateApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeHibernateApplication.class, args);
 	}
-
+@Override   
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+{         
+		return application.sources(EmployeeHibernateApplication.class); 
+}
 }
